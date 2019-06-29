89dad443735645de97c6c3e540c40a4c0efe1fe2
fshlarf <faishal.arif218@gmail.com>
2019-06-29 16:58:42 +0700

    design layout ui, make CurrentCurrency component

---
+++ components/CurrentCurrency.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="current-currency">
+    <h5 class="current-currency__title">{{ title }}</h5>
+    <div class="current-currency__content">
+      <div class="current-currency__content-name">
+        {{ nameCurrency }}
+      </div>
+      <div class="current-currency__content-nominal">
+        {{ nominal }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'USD - United States Dollars'
+    },
+    nameCurrency: {
+      type: String,
+      default: 'USD'
+    },
+    nominal: {
+      type: Number,
+      default: 10.00
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.current-currency {
+  padding: 16px;
+  &__title {
+
+  }
+  &__content {
+    display: flex;
+    &-nominal {
+      margin-left: auto;
+    }
+  }
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="container">
-    <button type="button" class="btn btn-primary">Primary</button>
+  <div class="">
+    <CurrentCurrency/>
     
   </div>
 </template>
 
 <script>
+import CurrentCurrency from '~/components/CurrentCurrency'
 import Store from '~/store/store'
 import { mapState, mapGetters, mapActions } from 'vuex'
 
 export default {
   store: Store,
   components: {
+    CurrentCurrency
   },
   mounted() {
     this.getFormatExCurrency()
@@ -29,33 +31,10 @@ export default {
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
